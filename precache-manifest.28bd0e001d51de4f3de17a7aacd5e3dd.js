a76dbc18e54ef08dc5f132c2c264d08f1eb70c22
Ashis Sarthak Singh <Ashissarthak.singh@refinitiv.com>
2019-12-10 14:28:07 +0530

    Updates

---
+++ precache-manifest.28bd0e001d51de4f3de17a7aacd5e3dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79de7df140b0b22a704ee323ed2152c",
+    "url": "/facerecog/index.html"
+  },
+  {
+    "revision": "c4781559822091b5d0ac",
+    "url": "/facerecog/static/css/main.06333350.chunk.css"
+  },
+  {
+    "revision": "2fdbd2ad43071ec1891d",
+    "url": "/facerecog/static/js/2.484707e1.chunk.js"
+  },
+  {
+    "revision": "cd98cac735833a4d908e58ff47ee8ea1",
+    "url": "/facerecog/static/js/2.484707e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4781559822091b5d0ac",
+    "url": "/facerecog/static/js/main.0811b365.chunk.js"
+  },
+  {
+    "revision": "d49347ba3de682639371",
+    "url": "/facerecog/static/js/runtime-main.fb2ab035.js"
+  }
+]);
